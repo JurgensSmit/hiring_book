78bd764dada3153dba314170542d379394fc3664
Jurgens Smit <contact@jurgenssmit.com>
2013-05-22 15:05:51 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HiringBook::Application.config.secret_token = 'c82dc45a750b6ba69df3d511a916e89ff8185f558ee0c94e590c65dba50a9e44979fa336154106e3585672dccd65d3423f601fade2aab3d0f72289782ab1505e'
