ce044d7e3f388c0440f31170da93282193a15e8e
David Evans <davidjsevans@gmail.com>
2013-05-24 11:59:05 +1000

    clean up home, projects, users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,25 @@ class UsersController < ApplicationController
       format.json { render json: @user }
     end
   end
+
+  def update
+    authorize! :update, @user, :message => 'Not authorized as an administrator.'
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user], :as => :admin)
+      redirect_to admin_path, :notice => "User updated."
+    else
+      redirect_to admin_path, :alert => "Unable to update user."
+    end
+  end
+
+  def destroy
+    authorize! :destroy, @user, :message => 'Not authorized as an administrator.'
+    user = User.find(params[:id])
+    unless user == current_user
+      user.destroy
+      redirect_to users_path, :notice => "User deleted."
+    else
+      redirect_to users_path, :notice => "Can't delete yourself."
+    end
+  end
 end
